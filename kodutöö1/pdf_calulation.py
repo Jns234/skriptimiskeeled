52974fc4731668c7e33d5197ecff744b15b84ebf
Jns234 <jankangsepp620@gmail.com>
2025-05-01 12:33:03 +0300

    fist task done

--- kodutöö1/pdf_calulation.py
+++ kodutöö1/pdf_calulation.py
@@ -1,15 +1,24 @@
 import os
 import pathlib
 
-#HOME_DIR = os.environ['HOME'] 
-HOME_DIR = pathlib.Path.home()
+HOME_DIR = os.environ['HOME'] 
+# HOME_DIR = pathlib.Path.home()
 
 def get_all_pdf_files(dir_path):
-    """Returns a list of all .pdf files starting from a root dir."""
+    """Returns a list of all .pdf files starting from a provided root dir."""
     files = []
     for path, subdirs, files in os.walk(dir_path):
         for name in files:
             if '.pdf' in name:
                 files.append(os.path.join(path, name))
 
-get_all_pdf_files(HOME_DIR)
+def calculate_size():
+    """Prints out the size of all .pdf files in MegaBytes."""
+    files = get_all_pdf_files(HOME_DIR)
+    size = 0
+    for file in files:
+        file_stats = os.stat(file)
+        size += file_stats.st_size
+    print(f'File Size in MegaBytes is {size / (1024 * 1024)}')
+
+calculate_size()
