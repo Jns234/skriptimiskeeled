a7bf9e9d6b538ab82200c4880d2ec95f4695f014
Jns234 <jankangsepp620@gmail.com>
2025-05-01 12:52:19 +0300

    add second task

--- kodutöö1/pdf_calulation.py
+++ kodutöö1/pdf_calulation.py
@@ -6,11 +6,12 @@ HOME_DIR = os.environ['HOME']
 
 def get_all_pdf_files(dir_path):
     """Returns a list of all .pdf files starting from a provided root dir."""
-    files = []
+    files_collected = []
     for path, subdirs, files in os.walk(dir_path):
         for name in files:
-            if '.pdf' in name:
-                files.append(os.path.join(path, name))
+            if name.endswith(".pdf") :
+                files_collected.append(os.path.join(path, name))
+    return files_collected
 
 def calculate_size():
     """Prints out the size of all .pdf files in MegaBytes."""
